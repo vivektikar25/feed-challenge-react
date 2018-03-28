eb08a5286f4d7292d4ea33beffff2362562b1888
Leigh Halliday <leighhalliday@gmail.com>
2018-03-28 12:20:43 -0400

    First and maybe only commit for this challenge starter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# FlipGive Feed Challenge: React
+
+Refer to https://github.com/BetterTheWorld/feed-challenge-api for details on the React side of this challenge and information about the API.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Activities from "./components/Activities";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Activities />
+      </div>
+    );
+  }
+}
+
+export default App;
