c980a5e70bbeea2ee4c25990c1561b3d3ab2149d
vivektikar@gmail.com <vivektikar@gmail.com>
2018-04-11 14:24:07 +0530

    Added snapshot tests

--- src/__tests__/Test.js
+++ src/__tests__/Test.js
@@ -1,9 +1,12 @@
 import React from "react";
 import DisplayPostCard from "./../Components/Feeds/PostedFeedCard/DisplayPostCard";
 import DisplaySharedCard from "./../Components/Feeds/SharedFeedCard/DisplaySharedCard";
+
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { shallow } from "enzyme";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -23,6 +26,8 @@ const ShareCardProp = {
   created_at: "2018-03-28T16:07:14.082Z",
   updated_at: "2018-03-28T16:07:14.082Z"
 };
+
+// Component Testing //
 
 it("Test post card without post", () => {
   const postedFeedCardRendered = shallow(
@@ -72,4 +77,70 @@ it("Test share card with url", () => {
   expect(sharedFeedCardRendered.contains("https://www.flipgive.com/")).toEqual(
     true
   );
+});
+
+// Snapshot Testing //
+
+it("renders DisplayPostCard without post", () => {
+  const tree = renderer
+    .create(
+      <MuiThemeProvider>
+        <DisplayPostCard
+          feed={PostCardProp}
+          cardsPostContent={""}
+          showCardsPostContentFlag={false}
+          showLoader={false}
+        />
+      </MuiThemeProvider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders DisplayPostCard with post", () => {
+  const tree = renderer
+    .create(
+      <MuiThemeProvider>
+        <DisplayPostCard
+          feed={PostCardProp}
+          cardsPostContent={"Want to grab pizza"}
+          showCardsPostContentFlag={true}
+          showLoader={false}
+        />
+      </MuiThemeProvider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders DisplaySharedCard without shared url", () => {
+  const tree = renderer
+    .create(
+      <MuiThemeProvider>
+        <DisplaySharedCard
+          feed={ShareCardProp}
+          cardsSharedUrl={""}
+          showCardsSharedUrlFlag={false}
+          showLoader={false}
+        />
+      </MuiThemeProvider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders DisplaySharedCard with shared url", () => {
+  const tree = renderer
+    .create(
+      <MuiThemeProvider>
+        <DisplaySharedCard
+          feed={ShareCardProp}
+          cardsSharedUrl={"https://www.flipgive.com/"}
+          showCardsSharedUrlFlag={true}
+          showLoader={false}
+        />
+      </MuiThemeProvider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
